4127537f1fb87302ee000fd46c20b9372b7fafcd
Marco <marco.rensch@area59.ch>
2017-01-30 15:05:32 +0100

    handling for uikit switcher
    added Case: resizing if  a videoframe is in a uikit (v2) switcher

--- tmpl/js/nx_script.js
+++ tmpl/js/nx_script.js
@@ -23,6 +23,10 @@ var nxvideobox = (function(){
 });
      
 jQuery(window).resize(nxvideobox);
+// Case: Player is in a switcher Box (yootheme uikit v.2)
+jQuery('[data-uk-switcher]').on('show.uk.switcher', function(event){
+    nxvideobox();
+});
 
 
 var tryCounter = 0;
